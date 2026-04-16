0337571ed29dc27db8aad34f8f49cb5a2be9327d
Labiokor <krisjodai@gmail.com>
2026-04-16 10:51:15 -0700

    updated server and new connection file for mongoose connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,46 @@
-require('dotenv').config()
+require('dotenv').config({path:'./config.env'});
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const financeRoutes = require('./routes/financeRoutes');
+const goalsRoutes = require('./routes/goalsRoutes');
+const habitsRoutes = require('./routes/habitsRoutes');
+const notesRoutes = require('./routes/notesRoutes');
+const reminderRoutes = require('./routes/reminderRoutes');
+const tasksRoutes = require('./routes/tasksRoutes');
+const {processRecurringReminders} = require('./Utils/ReminderScheduler')
 
-const mongoose = require('mongoose')
+const app = express();
 
-const app = require('./app')
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.ATLAS_URI)
-
-.then(()=>{
-
-  console.log('Database connected')
-
-  app.listen(5000,()=>{
-
-    console.log('Server running on port 5000')
-
-  })
-
-})
-
+//Mongoose connection
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+)
+.then(()=>console.log('Database connected'))
 .catch(err=>{
+ console.log('Error connecting to database:', err);})
 
-  console.log(err)
+//Routes
+app.use('/api/auth',authRoutes);
+app.use('/api/finance',financeRoutes);
+app.use('/api/goals',goalsRoutes);
+app.use('/api/habits',habitsRoutes);
+app.use('/api/notes',notesRoutes);
+app.use('/api/reminders',reminderRoutes);
+app.use('/api/tasks',tasksRoutes);
+app.use('/api/ReminderScheduler/processRecurring',processRecurringReminders);
 
-})
+//Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
